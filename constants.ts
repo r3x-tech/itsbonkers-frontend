2f7553336226543359a91a2b10501623fe8b4b0e
adrian-petrykiw <cucumber901@gmail.com>
2023-12-19 17:41:11 -0600

    addy update

--- constants.ts
+++ constants.ts
@@ -1,15 +1,20 @@
-export const GAME_ID = 1;
-export const TOKEN_MINT_ADDRESS = "";
-export const PROPULSION_MINT_ADDRESS = "";
-export const LANDING_GEAR_MINT_ADDRESS = "";
-export const NAVIGATION_MINT_ADDRESS = "";
-export const PRESENTS_BAG_MINT_ADDRESS = "";
+export const GAME_ID = 5;
+export const TOKEN_MINT_ADDRESS =
+  "Gx1V34ivZZ1Fq7Rm9ZmogBdDgYZieYKjJU1icSupFuCT";
+export const PROPULSION_MINT_ADDRESS =
+  "G6NJuVEa91vjctj7eviCwHCbvF4VummiF3HBnwWJhadc";
+export const LANDING_GEAR_MINT_ADDRESS =
+  "BT7Ba5NcawMXfFdXoXnbozRXZBJD4yCzuQZcmwTSGA2w";
+export const NAVIGATION_MINT_ADDRESS =
+  "DCZtg4Hw4fPSiV7RTL7k6nxRCbsmbSY6nCvmGrzTuULt";
+export const PRESENTS_BAG_MINT_ADDRESS =
+  "45Hu4ybQktgnLnrKMvq3K7dFd9zix6qh1SiJUJuUX894";
 
 export const SPL_TOKENS: {
   [token: string]: { mint: string; decimals: number };
 } = {
   bonk: {
-    mint: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
+    mint: "Gx1V34ivZZ1Fq7Rm9ZmogBdDgYZieYKjJU1icSupFuCT",
     decimals: 5,
   },
 };
