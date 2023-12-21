3eb113a1eeace95d9d58eda875c4e58855d184c0
adrian-petrykiw <cucumber901@gmail.com>
2023-12-21 11:42:34 -0600

    new game 7

--- constants.ts
+++ constants.ts
@@ -1,15 +1,15 @@
-export const GAME_ID = 16;
+export const GAME_ID = 17;
 export const TOKEN_MINT_ADDRESS =
   "Gx1V34ivZZ1Fq7Rm9ZmogBdDgYZieYKjJU1icSupFuCT";
 
 export const PROPULSION_MINT_ADDRESS =
-  "E7SG8UtAtypgYFVLwwYpjZjxkrz7fQvWGGudMufQhvm8";
+  "2pVHniM4zvfx8kTtHNFy6oWGyYrDZP49nKZBWpHeBtgS";
 export const LANDING_GEAR_MINT_ADDRESS =
-  "GW8pKDXTgeYyENSHE2wYTAMXp79QX1PA4A4h1wRWfUpC";
+  "HtQo6QzJKCFhoQ9kFz7vN4atSm4Yqq6KLE2jpJgoBbRJ";
 export const NAVIGATION_MINT_ADDRESS =
-  "5sYKLj5wwUth5yxBPTbzjd4TkWXgiwgBhVVLRG4Fhrs7";
+  "13RxfhgHRPoyA9WZpHEUHZ2aFNUuUfGgVh58Spz92LVd";
 export const PRESENTS_BAG_MINT_ADDRESS =
-  "G3iqWGTCmyQ3q9bfGnfyX3gdTuuzNmYUBxo7eZQGwexT";
+  "6jmn8cZp3nR1Vmf6E6nKWZbu6Kz3EaXq7N4ZL8XndVrx";
 
 export const SPL_TOKENS: {
   [token: string]: { mint: string; decimals: number };
